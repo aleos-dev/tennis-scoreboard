2b35c0b237f7bff0e1f5c97c8ae4903ada717ee9
Aleksei Os. <oal.aleos@gmail.com>
2024-08-10 17:01:13 +0300

    feat: add MatchScore class to manage score and history entries

--- src/main/java/com/aleos/model/MatchScore.java
+++ src/main/java/com/aleos/model/MatchScore.java
@@ -1,4 +1,52 @@
 package com.aleos.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+
 public class MatchScore {
+
+    @Getter
+    private final UUID matchID;
+
+    @Getter
+    private final String playerOneName;
+
+    @Getter
+    private final String playerTwoName;
+
+    @Getter
+    @Setter
+    private String[] scorePoints;
+
+    @Getter
+    @Setter
+    private String[] scoreGames;
+
+    @Getter
+    @Setter
+    private String[] scoreSets;
+
+    @Getter
+    private final List<String> historyEntries = new ArrayList<>();
+
+    private final Deque<String> notifications = new LinkedList<>();
+
+
+    public MatchScore(UUID matchID, String playerOneName, String playerTwoName) {
+        this.matchID = matchID;
+        this.playerOneName = playerOneName;
+        this.playerTwoName = playerTwoName;
+    }
+
+    public Optional<String> pollNotification() {
+        return Optional.ofNullable(notifications.poll());
+    }
+
+    public void addNotification(String message) {
+        notifications.offer(message);
+        historyEntries.add(message);
+    }
 }
