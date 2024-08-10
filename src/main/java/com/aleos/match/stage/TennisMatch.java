c89a4fbf7c442d7fcfd2016d65c6d67e5e6b5ac0
Aleksei Os. <oal.aleos@gmail.com>
2024-08-10 17:00:21 +0300

    ref: extend TennisMatch interface with additional methods

--- src/main/java/com/aleos/match/stage/TennisMatch.java
+++ src/main/java/com/aleos/match/stage/TennisMatch.java
@@ -1,5 +1,21 @@
 package com.aleos.match.stage;
 
-// Interface marker
+import java.util.Optional;
+import java.util.UUID;
+
 public interface TennisMatch extends Stage {
+
+    UUID getId();
+
+    void scorePoint(String player);
+
+    String getPlayerOneName();
+
+    String getPlayerTwoName();
+
+    void setPlayerOne(String playerOne);
+
+    void setPlayerTwo(String playerTwo);
+
+    Optional<String> getMatchWinner();
 }
