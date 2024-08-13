9c24a6430ba1072c7297e6332874401bf58d9912
Aleksei Os. <oal.aleos@gmail.com>
2024-08-13 16:28:55 +0300

    add InvalidPlayerException.java

---
+++ src/main/java/com/aleos/match/exception/InvalidPlayerException.java
@@ -0,0 +1,8 @@
+package com.aleos.match.exception;
+
+public class InvalidPlayerException extends RuntimeException {
+
+    public InvalidPlayerException(String message) {
+        super(message);
+    }
+}
