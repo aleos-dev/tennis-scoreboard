810801dfb70b49bcd0ab265f5436c27c10518efb
Aleksei Os. <oal.aleos@gmail.com>
2024-08-14 15:31:06 +0300

    ref: configure ObjectMapper for LocalDateTime support
    - Registered JavaTimeModule with ObjectMapper to handle LocalDateTime serialization.
    - Disabled WRITE_DATES_AS_TIMESTAMPS to use ISO-8601 format for dates.

--- src/main/java/com/aleos/configuration/AppConfiguration.java
+++ src/main/java/com/aleos/configuration/AppConfiguration.java
@@ -10,6 +10,8 @@ import com.aleos.service.MatchService;
 import com.aleos.service.ScoreTrackerService;
 import com.aleos.util.PropertiesUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.validation.Validation;
@@ -84,7 +86,11 @@ public class AppConfiguration {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return objectMapper;
     }
 }
 
