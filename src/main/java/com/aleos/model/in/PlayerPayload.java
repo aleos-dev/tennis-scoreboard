23922d4e288e56ac355980dd027c5306d9b096f4
Aleksei Os. <oal.aleos@gmail.com>
2024-08-16 19:18:43 +0300

    ref: convert PlayerNamePayload and PlayerPayload to a record and exclude them from BasicGroup validation

--- src/main/java/com/aleos/model/in/PlayerPayload.java
+++ src/main/java/com/aleos/model/in/PlayerPayload.java
@@ -1,19 +1,18 @@
 package com.aleos.model.in;
 
-import com.aleos.validation.BasicGroup;
 import com.aleos.validation.ValidCountryCode;
-import jakarta.servlet.http.Part;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public class PlayerPayload {
+public record PlayerPayload(
 
-    @NotNull(message = "Name cannot be null.")
-    @Size(min = 5, max = 50, groups = BasicGroup.class, message = "The name must be between {min} and {max}.")
-    String name;
+        @NotNull(message = "Name cannot be null.")
+        @Size(min = 5, max = 50, message = "The name must be between {min} and {max}.")
+        String name,
 
-    @ValidCountryCode
-    String country;
+        @ValidCountryCode
+        String country,
 
-    Part image;
+        String imageUrl
+) {
 }
