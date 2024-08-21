9b786dde9664f65895fb86f6068579fffdc9412b
Aleksei Os. <oal.aleos@gmail.com>
2024-08-21 17:55:23 +0300

    feat: add NewMatchServlet to handle match creation request as a facade

---
+++ src/main/java/com/aleos/servlet/NewMatchServlet.java
@@ -0,0 +1,16 @@
+package com.aleos.servlet;
+
+import com.aleos.util.ServletUtil;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/new-match")
+public class NewMatchServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        ServletUtil.forwardToJsp(req, resp, "control/createMatch");
+    }
+}
