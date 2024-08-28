954fdc6c193504826575670144932a376e630911
Aleksei Os. <oal.aleos@gmail.com>
2024-08-28 19:01:01 +0300

    feat: add help methods buildQueryString and getBeforeDateAttribute to set attributes which will be used by jsp pages

--- src/main/java/com/aleos/servlet/MatchesServlet.java
+++ src/main/java/com/aleos/servlet/MatchesServlet.java
@@ -15,6 +15,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.StringJoiner;
 import java.util.UUID;
 
 @WebServlet("/matches")
@@ -32,21 +35,58 @@ public class MatchesServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        if (req.getAttribute("violations") != null) {
+            ServletUtil.forwardToJsp(req, resp, "control/matches");
+            return;
+        }
+
         var pageable = (PageableInfo) req.getAttribute("pageablePayload");
         var filterCriteria = (MatchFilterCriteria) req.getAttribute("matchFilterCriteria");
 
+
         MatchesDto matchesDto = matchService.findAll(pageable, filterCriteria);
         req.setAttribute("matchesDto", matchesDto);
+        req.setAttribute("filterCriteria", filterCriteria);
+        req.setAttribute("pageable", pageable);
+
+        // Build the query parameters (excluding pagination)
+        req.setAttribute("baseUrl", req.getContextPath() + req.getServletPath());
+        req.setAttribute("queryParam", buildQueryString(filterCriteria));
 
         ServletUtil.forwardToJsp(req, resp, "control/matches");
     }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        var payload = (MatchPayload) req.getAttribute("matchPayload");
+        if (req.getAttribute("violations") != null) {
+            ServletUtil.forwardToJsp(req, resp, "control/create-match");
+            return;
+        }
 
+        var payload = (MatchPayload) req.getAttribute("matchPayload");
         UUID matchId = matchService.createMatch(payload);
 
         ServletUtil.redirect(req, resp, "/matches/%s".formatted(matchId));
+    }
+
+    private String buildQueryString(MatchFilterCriteria filterCriteria) {
+        StringJoiner queryParams = new StringJoiner("&", "?", "");
+
+        if (filterCriteria.playerName() != null && !filterCriteria.playerName().isEmpty()) {
+            queryParams.add("playerName=" + filterCriteria.playerName());
+        }
+        if (filterCriteria.status() != null && !filterCriteria.status().isEmpty()) {
+            queryParams.add("status=" + filterCriteria.status());
+        }
+
+        queryParams.add("before=" + getBeforeDateAttribute(filterCriteria));
+
+        return queryParams.toString();
+    }
+
+    private String getBeforeDateAttribute(MatchFilterCriteria filterCriteria) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
+                .withZone(ZoneId.systemDefault());
+        return formatter.format(filterCriteria.before());
     }
 }
