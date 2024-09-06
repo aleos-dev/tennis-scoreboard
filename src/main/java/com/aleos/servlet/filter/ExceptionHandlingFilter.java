fb703cecbf38bf2247e6f1711e4102734f16b825
Aleksei O <oal.aleos@gmail.com>
2024-09-06 19:47:19 +0300

    ref: catch and handle produced custom exception

--- src/main/java/com/aleos/servlet/filter/ExceptionHandlingFilter.java
+++ src/main/java/com/aleos/servlet/filter/ExceptionHandlingFilter.java
@@ -1,9 +1,11 @@
 package com.aleos.servlet.filter;
 
+import com.aleos.exception.ImageServiceException;
+import com.aleos.exception.InvalidPageableRequest;
+import com.aleos.exception.InvalidPathInfo;
 import com.aleos.exception.PlayerRegistrationException;
 import com.aleos.util.ServletUtil;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,23 +20,22 @@ public class ExceptionHandlingFilter extends HttpFilter {
     private static final Logger logger = Logger.getLogger(ExceptionHandlingFilter.class.getName());
 
     @Override
-    public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
+    public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException {
         try {
-
             chain.doFilter(req, resp);
 
-        } catch (PlayerRegistrationException e) {
+        } catch (InvalidPageableRequest
+                 | InvalidPathInfo
+                 | PlayerRegistrationException
+                 | ImageServiceException e) {
+
             logger.log(Level.SEVERE, e.getMessage(), e);
             req.setAttribute("errorMessages", List.of(e.getMessage()));
-
             ServletUtil.forward(req, resp, req.getServletPath());
 
         } catch (Exception e) {
-
-            logger.log(Level.SEVERE, e.getMessage(), e);
-            resp.setStatus(500);
-
-            req.getRequestDispatcher("/errorPage.jsp").forward(req, resp);
+            logger.log(Level.SEVERE, "Something went wrong!", e);
+            resp.sendError(500, "Oops! Looks like the gremlins are at it again. We'll fix it soon!");
         }
     }
 }
