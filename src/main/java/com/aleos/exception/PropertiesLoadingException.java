4bbb8a3670c035a1b132db48d976d29923a22790
Aleksei O <oal.aleos@gmail.com>
2024-09-06 19:44:23 +0300

    ref: code cleanup and minor refactor

--- src/main/java/com/aleos/exception/PropertiesLoadingException.java
+++ src/main/java/com/aleos/exception/PropertiesLoadingException.java
@@ -1,9 +1,8 @@
 package com.aleos.exception;
 
-import java.io.IOException;
-
 public class PropertiesLoadingException extends RuntimeException {
 
-    public PropertiesLoadingException(String failedToLoadPropertiesFile, IOException e) {
+    public PropertiesLoadingException(String message, Exception e) {
+        super(e);
     }
 }
