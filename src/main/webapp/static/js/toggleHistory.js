a473b4e8cd79a14339c63a177c7851878a657365
Aleksei Os. <oal.aleos@gmail.com>
2024-09-07 16:06:20 +0300

    ref: update toggleHistory func to no change footer css

--- src/main/webapp/static/js/toggleHistory.js
+++ src/main/webapp/static/js/toggleHistory.js
@@ -1,12 +1,8 @@
 function toggleHistory() {
     let historyDiv = document.getElementById('matchHistory');
-    let footer = document.querySelector('.site-footer');
-    if (footer.style.position !== 'sticky') {
-        footer.style.position = 'sticky';
-        footer.style.bottom = '0';
+    if (historyDiv.style.display === 'none' || historyDiv.style.display === '') {
         historyDiv.style.display = 'block';
     } else {
-        footer.style.position = ''
         historyDiv.style.display = 'none';
     }
 }
