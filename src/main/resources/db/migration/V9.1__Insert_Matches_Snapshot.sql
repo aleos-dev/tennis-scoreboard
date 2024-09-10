c644094333718a0d0736b86a3ae1306394f53574
Aleksei Os. <oal.aleos@gmail.com>
2024-09-10 10:59:03 +0300

    fix: correct data for the match snapshot

--- src/main/resources/db/migration/V9.1__Insert_Matches_Snapshot.sql
+++ src/main/resources/db/migration/V9.1__Insert_Matches_Snapshot.sql
@@ -1,6 +1,6 @@
 -- Match 1: Serena Williams vs Roger Federer
 UPDATE match
-SET final_score_record = '6:3,5(4):7(6),4:6,2:6,5:7'
+SET final_score_record = '6:3,6(4):7(6),4:6,2:6,5:7'
 WHERE id = 'ff41b2e8-130e-4756-a35c-7ff889b07a11';
 
 -- Match 2: Naomi Osaka vs Rafael Nadal
@@ -15,7 +15,7 @@ WHERE id = '1495f284-26f7-40cf-966a-0d4f7cd546f0';
 
 -- Match 4: Ashleigh Barty vs Dominic Thiem
 UPDATE match
-SET final_score_record = '7(6):6,4:6,3:6'
+SET final_score_record = '7(6):6(2),4:6,3:6'
 WHERE id = '82ac6e9a-daf5-4ab3-9e80-bf039b483efb';
 
 -- Match 5: Venus Williams vs Andy Murray
@@ -35,7 +35,7 @@ WHERE id = '865af6fb-2370-4c21-a11e-45b68126de0a';
 
 -- Match 8: Rafael Nadal vs Simona Halep
 UPDATE match
-SET final_score_record = '6:4,6(6):7(7),6:3'
+SET final_score_record = '6:4,6(5):7(7),6:3'
 WHERE id = 'c7443a21-57af-4823-8f60-b1158fb6d6a3';
 
 -- Match 9: Novak Djokovic vs Ashleigh Barty
@@ -45,7 +45,7 @@ WHERE id = '3bd226ba-c416-4e4a-86c8-ccf295d46bf6';
 
 -- Match 10: Dominic Thiem vs Venus Williams
 UPDATE match
-SET final_score_record = '6:3,7(6):6,6:4'
+SET final_score_record = '6:3,7(6):6(3),6:4'
 WHERE id = '3299b9fa-f10b-48e9-96bd-08acfe87b811';
 
 -- Match 11: Andy Murray vs Maria Sharapova
@@ -55,7 +55,7 @@ WHERE id = 'c651a4df-5061-4e2a-aa0c-e5ec75e3fe6e';
 
 -- Match 12: Serena Williams vs Naomi Osaka (2020)
 UPDATE match
-SET final_score_record = '6:3,6(6):7(4),6:4'
+SET final_score_record = '6:3,6(4):7(6),6:4'
 WHERE id = 'd1f5d1e1-fd5c-4b0a-bb2d-1c8b45f7a3e2';
 
 -- Match 13: Roger Federer vs Rafael Nadal (2020)
